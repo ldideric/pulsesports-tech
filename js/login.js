a4a86eb6c6b2ca1d83bcc1fd21305cb0f4f23f7e
Lietze Diderich <l.o.diderich@hotmail.com>
2022-11-21 20:25:04 +0100

    added minor js, reworked login page

---
+++ js/login.js
@@ -0,0 +1,5 @@
+
+function saveLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+}
---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+// Adding navigate function to navbar
+document.getElementById('navbar-survey').addEventListener('click', () => {
+    window.location.href = './survey.html';
+});
+document.getElementById('navbar-overview').addEventListener('click', () => {
+    window.location.href = './overview.html';
+});
+document.getElementById('navbar-home').addEventListener('click', () => {
+    window.location.href = './index.html';
+});
