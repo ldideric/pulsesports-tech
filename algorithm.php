e5dcc111c83f3bea72bcc9631e85ecb8a07586c4
Lietze Diderich <l.o.diderich@hotmail.com>
2022-11-21 14:02:01 +0100

    rm database

---
+++ algorithm.php
@@ -0,0 +1,26 @@
+<?php
+
+// Heart Rate (bpm)
+$HR;
+
+// Heart Rate Variability (bpm)
+$HRV;
+
+// Resting Heart Rate (bpm)
+$RHR;
+
+// Sleep Time (hours)
+$ST;
+
+
+// Heart Rate Max (bpm)
+$HRM;
+//220 - $age;
+
+// Heart Rate Zones (1-5)
+$HRZ;
+// 1: 50%-60% HRM
+// 2: 60%-70% HRM
+// 3: 70%-80% HRM
+// 4: 80%-90% HRM
+// 5: 90%-100% HRM
