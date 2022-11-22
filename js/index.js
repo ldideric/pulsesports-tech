22d54964d9a5319d3a8120644e3971c8b78c4400
Kian Acquoij <kian.acquoij@student.rocva.nl>
2022-11-22 16:04:04 +0100

    many things edited

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 function saveLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    window.location.href = 'home.html';
+    // window.location.href = 'home.html';
     transitionLogo();
 }
 
 function transitionLogo() {
     const logo = document.querySelector('.logo-img');
     const logostyle = window.getComputedStyle(logo);
+    const logoparent = logo.parentElement;
+    const logoparentstyle = window.getComputedStyle(logoparent);
 
     const currentls = JSON.parse(localStorage.getItem('logotransition')) || {};
-    currentls.height = logo.height;
-    currentls.width = logo.width;
-    currentls.margintop = logostyle.marginTop;
+    currentls.height = logoparent.height;
+    currentls.width = logoparent.width;
+    currentls.padding = logoparentstyle.padding;
     localStorage.setItem('logotransition', JSON.stringify(currentls));
 }
