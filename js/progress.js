a641f5cb06afd945b944bca426ebe0c7e7bd3c7e
dj_quinten <quinten1vanderlaan@outlook.com>
2022-11-23 17:02:01 +0100

    newhome

--- js/progress.js
+++ js/progress.js
@@ -27,19 +27,15 @@ function CreateHudWork2(className, color, trailColor){
 SetupHud = function() {
     WorkCircle = CreateHudWork('WorkCapacityCircle', `rgb(64,64,64)`, 'rgb(248,248,248)');
     WorkCircle.animate(0.4);
-
     PulseCircle = CreateHudWork('StepsCircle', `rgb(252,15,78)`, 'rgb(248,248,248)');
     PulseCircle.animate(0.6);
-
     KCALCircle = CreateHudWork('KCALCircle', `rgb(0,164,255)`, 'rgb(248,248,248)');
     KCALCircle.animate(0.8);
 
     PulseIndexCircle = CreateHudWork2('pulse-index-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
     PulseIndexCircle.animate(0.8);
-
     HeartRateCircle = CreateHudWork2('HeartRate-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
     HeartRateCircle.animate(0.8);
-
     SleepTimeCircle = CreateHudWork2('SleepTime-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
     SleepTimeCircle.animate(0.8);
 }
