0ae7b794648566ab3ba67246ed54902b787ac408
Lietze Diderich <lietze14@gmail.com>
2022-11-23 20:33:25 +0100

    reworked graphs, added navbar w/ profile to pages

--- js/progress.js
+++ js/progress.js
@@ -1,11 +1,18 @@
-let WorkCircle, PulseCircle, KCALCircle, PulseIndexCircle, HeartRateCircle, SleepTimeCircle;
+let WorkCircle, PulseCircle, KCALCircle, PulseIndexCircle, HeartRateCircle, SleepTimeCircle, userData;
+const url = "./userdata.json";
+
+async function getUserData() {
+    const response = await fetch(url);
+    userData = await response.json();
+    return userData;
+}
 
 function CreateHudWork(className, color, trailColor){
     return new ProgressBar.Circle(`.${className}`, {
         color: color,
         trailColor: trailColor,
-        strokeWidth: 23,
-        trailWidth: 23,
+        strokeWidth: 25,
+        trailWidth: 10,
         duration: 2000,
         easing: "easeInOut",
         fill: "transparent",
@@ -16,34 +23,37 @@ function CreateHudWork2(className, color, trailColor){
     return new ProgressBar.Circle(`.${className}`, {
         color: color,
         trailColor: trailColor,
-        strokeWidth: 12,
-        trailWidth: 12,
+        strokeWidth: 20,
+        trailWidth: 20,
         duration: 2000,
         easing: "easeInOut",
         fill: "transparent",
     });
 }
 
-SetupHud = function() {
-    WorkCircle = CreateHudWork('WorkCapacityCircle', `rgb(64,64,64)`, 'rgb(248,248,248)');
-    WorkCircle.animate(0.4);
+SetupHud = async function() {
+    userData = await getUserData();
+    userData = userData.admin.timeline['01-01-2000'].statistics;
 
-    PulseCircle = CreateHudWork('StepsCircle', `rgb(252,15,78)`, 'rgb(248,248,248)');
-    PulseCircle.animate(0.6);
+    WorkCircle = CreateHudWork('WorkCapacityCircle', `rgb(64,64,64)`, 'rgb(225,225,225)');
+    WorkCircle.animate(0.78);
 
-    KCALCircle = CreateHudWork('KCALCircle', `rgb(0,164,255)`, 'rgb(248,248,248)');
-    KCALCircle.animate(0.8);
+    PulseCircle = CreateHudWork('StepsCircle', `rgb(252,15,78)`, 'rgb(225,225,225)');
+    PulseCircle.animate(0.4);
 
-    PulseIndexCircle = CreateHudWork2('pulse-index-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
-    PulseIndexCircle.animate(0.8);
+    KCALCircle = CreateHudWork('KCALCircle', `rgb(0,164,255)`, 'rgb(225,225,225)');
+    KCALCircle.animate(0.6);
 
-    HeartRateCircle = CreateHudWork2('HeartRate-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
-    HeartRateCircle.animate(0.8);
+    PulseIndexCircle = CreateHudWork2('pulse-index-circle', `rgb(54, 139, 51)`, 'rgb(135, 189, 133)');
+    PulseIndexCircle.animate(0.2);
 
-    SleepTimeCircle = CreateHudWork2('SleepTime-circle', `rgb(65,114,202)`, 'rgb(84,130,53)');
-    SleepTimeCircle.animate(0.8);
+    let HeartRate = userData.HeartRate / userData.HRMax;
+    HeartRateCircle = CreateHudWork2('HeartRate-circle', `rgb(180, 54, 31)`, 'rgb(240, 129, 110)');
+    HeartRateCircle.animate(HeartRate);
+
+    let SleepTime = userData.SleepTime / 8;
+    SleepTimeCircle = CreateHudWork2('SleepTime-circle', `rgb(65,114,202)`, 'rgb(144, 172, 224)');
+    SleepTimeCircle.animate(SleepTime);
+
 }
-
-
-
 SetupHud()
